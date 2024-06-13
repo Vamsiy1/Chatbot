cb623b60a14665f4cc4f6645ad243d8c0c7069c4
Vamsiy1 <118638883+Vamsiy1@users.noreply.github.com>
2024-06-13 09:14:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ print('VALUE OF TOKEN -->',huggingfacehub_api_token)
 # Initialize the HuggingFaceEndpoint with the conversation buffer memory
 llm = HuggingFaceEndpoint(
     repo_id="openai-community/gpt2",
-    max_new_tokens=1024,
+    max_new_tokens=512,
     top_k=10,
     top_p=0.95,
     typical_p=0.95,
